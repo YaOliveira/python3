06c2d40c76e50f8c6dfaddba25b5e934120f77ec
Yara Oliveira <magicsunshine_ya@msn.com>
2020-02-16 20:43:12 -0300

    list and index

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,6 +3,24 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                # print("Encontrei a letra {} na posição {}".format(letra, index))
+                letras_acertadas[index] = letra
+            index = index + 1
+        print(letras_acertadas)
+        
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
