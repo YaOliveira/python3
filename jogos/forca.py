16e1664901dfc5c58684257d24ad3711865d7c0e
Yara Oliveira <magicsunshine_ya@msn.com>
2020-02-17 00:04:42 -0300

    create palavras.txt

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,34 +1,40 @@
 def jogar():
-
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana".upper()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
-    enforcou = False
-    acertou = False
     erros = 0
-
+    print(len(palavra_secreta))
     print(letras_acertadas)
 
-    while (not acertou and not enforcou):
+    while(True):
 
         chute = input("Qual letra? ")
         chute = chute.strip().upper()
 
-        if (chute in palavra_secreta):
+        if(chute in palavra_secreta):
             index = 0
             for letra in palavra_secreta:
-                if (chute == letra):
+                if(chute == letra):
                     letras_acertadas[index] = letra
                 index += 1
         else:
             erros += 1
 
-        enforcou = erros == 6
+        if (erros == 6):
+            break
+        if ("_" not in letras_acertadas):
+            break
         print(letras_acertadas)
+
+
+    if("_" not in letras_acertadas):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
 
     print("Fim do jogo")
 
