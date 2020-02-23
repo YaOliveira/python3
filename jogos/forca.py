63e4f8ff566123540389fb59f834d1958884f662
Yara Oliveira <magicsunshine_ya@msn.com>
2020-02-23 20:59:28 -0300

    forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,54 +1,55 @@
 import random
 
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    arquivo = open('palavras.txt', 'r')
+    arquivo = open("palavras.txt", "r")
     palavras = []
 
     for linha in arquivo:
-        linha.strip()
-        palavras.append()
+        linha = linha.strip()
+        palavras.append(linha)
 
     arquivo.close()
-    
-    numero = random.randrange(0,len(palavras))
+
+    numero = random.randrange(0, len(palavras))
     palavra_secreta = palavras[numero].upper()
+
     letras_acertadas = ["_" for letra in palavra_secreta]
 
+    enforcou = False
+    acertou = False
     erros = 0
-    print(len(palavra_secreta))
+
     print(letras_acertadas)
 
-    while(True):
+    while not enforcou and not acertou:
 
         chute = input("Qual letra? ")
         chute = chute.strip().upper()
 
-        if(chute in palavra_secreta):
+        if chute in palavra_secreta:
             index = 0
             for letra in palavra_secreta:
-                if(chute == letra):
+                if chute == letra:
                     letras_acertadas[index] = letra
                 index += 1
         else:
             erros += 1
 
-        if (erros == 6):
-            break
-        if ("_" not in letras_acertadas):
-            break
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
-
-    if("_" not in letras_acertadas):
+    if acertou:
         print("Você ganhou!!")
     else:
         print("Você perdeu!!")
-
     print("Fim do jogo")
 
-if(__name__ == "__main__"):
+
+if __name__ == "__main__":
     jogar()
